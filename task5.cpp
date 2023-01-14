a2ec4e20364a05a709b58c4049520e4cfd75586d
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-14 16:36:21 +0500

    In this program user enter a number then program will display whether the number is odd or even.

---
+++ task5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int number;
+    cout<<"Enter a number";
+    cin>>number;
+    if(number%2==0)
+    {
+      cout<<" Number is even "; 
+    }
+    if(number%2!=0)
+    {
+      cout<<"Number is odd";
+    }
+  
+
+}
