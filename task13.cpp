092e3e04b3afd9f985c6fd1a05ecd83669f8f0c3
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-14 16:49:51 +0500

    In this program user enter the two numbers and greater number between them will be displayed.

---
+++ task13.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int num1,num2;
+    cout<<"Enter number 1 ";
+    cin>>num1;
+    cout<<"Enter number 2 ";
+    cin>>num2;
+    if(num1>num2)
+    {
+        cout<<num1<<" Number 1 is  greater ";
+    }
+    else 
+    {
+        cout<<num2<<" Number 2 is greater ";
+    }
+}
