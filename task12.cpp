031a925a48086261833bc2c1eec6cf85c2e1872b
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-14 16:48:24 +0500

    This program will display "you are going to University" if friends are going and there is class today.

---
+++ task12.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    string class1;
+    string friend1;
+    cout<<"Enter Yes if there is class:";
+    cin>>class1;
+    cout<<"Enter yes if friends are going:";
+    cin>>friend2;
+    if(class1=="Yes")
+    {
+        if(friend1=="Yes")
+        {
+            cout<<"You are going";
+        }
+    }
+}
