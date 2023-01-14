5f39943a2f44f4566efe44c90569e24289a3967a
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-14 16:31:55 +0500

    This program will show that you will buy the dress if conditions are matched

---
+++ task1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int price;
+    string brand;
+    cout<<"Enter price";
+    cin>>price;
+    cout<<"Enter brand";
+    cin>>brand;
+    if(price>=1500&&brand=="breakout");
+    {
+        cout<<"buy the dress";
+    }
+}
