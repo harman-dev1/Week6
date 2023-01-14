d7fb0927040d04bc12fc3be973e41a24a1f85b3e
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-14 16:33:22 +0500

    In this program user enter roll no name if name matched certain conditions then output will be congrats.

---
+++ task2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main(){
+    int rollNo;
+    string name;
+    cout<<"Enter your name:";
+    cin>>name;
+    cout<<"Enter your roll no:";
+    cin>>rollNo;
+    if(name=="ali")
+    {
+        if(rollNo==501)
+        {
+         cout<<"Congratulation";
+         }
+       
+    }
+}
